f8228abba002d008626c716970c5fead07b97229
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-01-18 15:20:15 -0600

    remove js for better code to toggle theme swtich

--- todo-app-main/todo.js
+++ todo-app-main/todo.js
@@ -8,76 +8,11 @@ const domController = (function () {
   function switchToDarkTheme() {
     themeToggle.setAttribute("src", "images/icon-sun.svg");
     staticDOM.pageBody.classList.add("dark");
-
-    document.documentElement.style.setProperty(
-      "--page-background",
-      "var(--darktheme-very-dark-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-background-color",
-      "var(--darktheme-very-dark-desaturated-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-text-color",
-      "var( --darktheme-light-grayish-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-bottom-border-color",
-      "var(--darktheme-very-dark-grayish-blue-lighter)"
-    );
-    document.documentElement.style.setProperty(
-      "--status-border-color",
-      "var(--darktheme-very-dark-grayish-blue-lighter)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-selection-text-hover",
-      "var(--darktheme-light-grayish-blue-hover)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-selection-text",
-      "var(--darktheme-dark-grayish-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--instruction-text-color",
-      "var(--darktheme-dark-grayish-blue)"
-    );
   }
 
   function switchToLightTheme() {
     themeToggle.setAttribute("src", "images/icon-moon.svg");
     staticDOM.pageBody.classList.remove("dark");
-    document.documentElement.style.setProperty(
-      "--page-background",
-      "var(--lighttheme-very-light-grayish-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-background-color",
-      "var(--lighttheme-very-light-gray)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-text-color",
-      "var(--lighttheme-very-dark-grayish-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-bottom-border-color",
-      "var(--lighttheme-very-light-grayish-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--status-border-color",
-      "var(--lighttheme-light-grayish-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-selection-text-hover",
-      "var(--lighttheme-very-dark-grayish-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--task-selection-text",
-      "var(--lighttheme-dark-grayish-blue)"
-    );
-    document.documentElement.style.setProperty(
-      "--instruction-text-color",
-      "var(--lighttheme-dark-grayish-blue)"
-    );
   }
 
   //   Set Static Event Listeners
