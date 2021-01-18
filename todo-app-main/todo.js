6988189e95f1b6ecf1c50344c53572f72648ad97
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-01-18 14:14:41 -0600

    add functions to switch between light and dark modes

--- todo-app-main/todo.js
+++ todo-app-main/todo.js
@@ -4,13 +4,81 @@ const domController = (function () {
     themeToggle: document.querySelector("#themeToggle"),
   };
 
-  function switchTheme() {
+  function switchToDarkTheme() {
+    themeToggle.setAttribute("src", "images/icon-sun.svg");
     document.documentElement.style.setProperty(
       "--page-background",
+      "var(--darktheme-very-dark-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-background-color",
       "var(--darktheme-very-dark-desaturated-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-text-color",
+      "var( --darktheme-light-grayish-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-bottom-border-color",
+      "var(--darktheme-very-dark-grayish-blue-lighter)"
+    );
+    document.documentElement.style.setProperty(
+      "--status-border-color",
+      "var(--darktheme-very-dark-grayish-blue-lighter)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-selection-text-hover",
+      "var(--darktheme-light-grayish-blue-hover)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-selection-text",
+      "var(--darktheme-dark-grayish-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--instruction-text-color",
+      "var(--darktheme-dark-grayish-blue)"
+    );
+  }
+
+  function switchToLightTheme() {
+    themeToggle.setAttribute("src", "images/icon-moon.svg");
+    document.documentElement.style.setProperty(
+      "--page-background",
+      "var(--lighttheme-very-light-grayish-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-background-color",
+      "var(--lighttheme-very-light-gray)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-text-color",
+      "var(--lighttheme-very-dark-grayish-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-bottom-border-color",
+      "var(--lighttheme-very-light-grayish-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--status-border-color",
+      "var(--lighttheme-light-grayish-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-selection-text-hover",
+      "var(--lighttheme-very-dark-grayish-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--task-selection-text",
+      "var(--lighttheme-dark-grayish-blue)"
+    );
+    document.documentElement.style.setProperty(
+      "--instruction-text-color",
+      "var(--lighttheme-dark-grayish-blue)"
     );
   }
 
   //   Set Static Event Listeners
-  staticDOM.themeToggle.addEventListener("click", switchTheme);
+  staticDOM.themeToggle.addEventListener("click", function (e) {
+    if (this.getAttribute("src").includes("moon")) switchToDarkTheme();
+    else switchToLightTheme();
+  });
 })();
