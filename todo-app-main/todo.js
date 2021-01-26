76563c8a4d1b247203f503111355bda0888ad0d7
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-01-26 15:04:50 -0600

    start adding syntax to insert new task DOM element

--- todo-app-main/todo.js
+++ todo-app-main/todo.js
@@ -3,6 +3,9 @@ const domController = (function () {
   const staticDOM = {
     themeToggle: document.querySelector("#themeToggle"),
     pageBody: document.querySelector("body"),
+    inputNewTask: document.querySelector("#inputNewTask"),
+    inputName: document.querySelector("#newTask"),
+    inputStatus: document.querySelector("#inputStatus"),
   };
 
   function switchToDarkTheme() {
@@ -17,9 +20,34 @@ const domController = (function () {
     staticDOM.pageBody.dataset.theme = "light";
   }
 
+  function insertNewTask(taskName, taskStatus) {
+    console.log(taskName, taskStatus);
+    const newTask = `
+    <div class="task">
+      <label class="task-status">
+        <input type="checkbox" />
+        <span class="checkmark"></span>
+      </label>
+      <span class="task-name">${taskName}</span>
+      <img
+      class="task-delete"
+      src="images/icon-cross.svg"
+      alt="Delete Task Button"
+      />
+    </div>
+    `;
+  }
+
   //   Set Static Event Listeners
   staticDOM.themeToggle.addEventListener("click", function (e) {
     if (this.getAttribute("src").includes("moon")) switchToDarkTheme();
     else switchToLightTheme();
+  });
+
+  staticDOM.inputNewTask.addEventListener("submit", function (e) {
+    e.preventDefault();
+    insertNewTask(staticDOM.inputName.value, staticDOM.inputStatus.checked);
+    staticDOM.inputName.value = "";
+    staticDOM.inputStatus.checked = false;
   });
 })();
