b624f94783cef8479715200a0ab5290ee4e8089b
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-03-02 14:41:58 -0600

    begin adding js to show pledge confirmation input when pledge is clicked on

--- crowdfunding-product-page-main/index.js
+++ crowdfunding-product-page-main/index.js
@@ -19,11 +19,38 @@
 (() => {
   const body = document.querySelector("body");
   const pledgeModal = document.querySelector("#pledge-modal");
+  const pledgeButton = document.querySelector("#pledge-button");
+  const pledgeForms = document.querySelectorAll("form[class*='pledge']");
 
   const displayPledgeModal = () => {
     body.classList.toggle("dark");
     pledgeModal.classList.toggle("show-modal");
   };
+
+  const displayPledgeConfirmation = (e) => {
+    if (e.target.tagName === "INPUT")
+      showConfirmation(
+        e.target.parentElement.parentElement.parentElement,
+        e.target.checked
+      );
+  };
+
+  const showConfirmation = (form, status) => {
+    if (status) {
+      document
+        .querySelectorAll("form[class*=pledge] input[type=checkbox]")
+        .forEach((check) => {
+          check.classList.remove("checked");
+          check.checked = false;
+        });
+      form.classList.add("checked");
+    } else form.classList.remove("checked");
+  };
+
+  pledgeButton.addEventListener("click", displayPledgeModal);
+  pledgeForms.forEach((form) =>
+    form.addEventListener("click", displayPledgeConfirmation)
+  );
 })();
 
 // Give Z-index only when clicked, meanign remove them for now
