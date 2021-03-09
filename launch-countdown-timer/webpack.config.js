d5378771b77782e089d980ea9684ff6d8cd6b9d9
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-03-09 12:40:02 -0600

    set up initial development environment with webpack, isntall sass,css,sytle loader along with html webpack plugin

---
+++ launch-countdown-timer/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "launch.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+};
