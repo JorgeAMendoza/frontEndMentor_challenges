556c8e9834900f9da77c3c4e395ed4e3d5107545
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-03-25 14:47:53 -0500

    remove comments from inital DOM creation

---
+++ launch-countdown-timer/src/app/utils/create-clock-part.js
@@ -0,0 +1,28 @@
+export const createClockPart = (timeValue, nextTimeValue, className = "") => {
+  const cardContainer = document.createElement("div");
+  cardContainer.setAttribute(
+    "class",
+    `countdown-clock__time__card ${className}`
+  );
+
+  const frontCard = document.createElement("div");
+  frontCard.setAttribute("class", "countdown-clock__time__card__front");
+
+  const backCard = document.createElement("div");
+  backCard.setAttribute("class", "countdown-clock__time__card__back");
+
+  const frontText = document.createElement("p");
+  frontText.textContent = `${timeValue}`;
+
+  const backText = document.createElement("p");
+  backText.textContent = `${nextTimeValue}`;
+
+  // Put it all together
+  frontCard.append(frontText);
+  backCard.append(backText);
+
+  cardContainer.append(frontCard);
+  cardContainer.append(backCard);
+
+  return cardContainer;
+};
