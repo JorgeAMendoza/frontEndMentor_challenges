cffe58c2af8e4859af61ee1474fab14aeaf23485
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-03-31 14:18:49 -0500

    creat function to stop countdown when time is hit, if past time when page loads then call function to set dom to launch status

--- launch-countdown-timer/src/app/clock.js
+++ launch-countdown-timer/src/app/clock.js
@@ -7,33 +7,18 @@ const dom = clockDOM();
 
 const clock = () => {
   // Set up target Date Object
-  const _targetDate = new Date("January 01, 2022");
+  const _targetDate = new Date("January 1, 2022");
   const _targetDay = Math.floor((_targetDate - new Date()) / 1000 / 86400);
-  const _targeDateData = {
-    days: _targetDay,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-  };
+
+  let _startCountdown;
 
   //   Set up current Date object
   const _currentDate = getCurrentTime();
 
-  dom.setInitialTime({
-    second: _currentDate.seconds,
-    nextSecond: _currentDate.seconds - 1 === -1 ? 59 : _currentDate.seconds - 1,
-    minute: _currentDate.minutes,
-    nextMinute: _currentDate.minutes - 1 === -1 ? 59 : _currentDate.minutes - 1,
-    hour: _currentDate.hours,
-    nextHour: _currentDate.hours - 1 === -1 ? 23 : _currentDate.hours - 1,
-    day: _currentDate.days,
-    nextDay: _currentDate.days - 1 === -1 ? 364 : _currentDate.days - 1,
-  });
-
   const changeTime = () => {
-    if (Date.now() <= _targetDate) {
-      // Call function to set DOM to be launched;
+    if (Date.now() >= _targetDate) {
       clearInterval(startCountdown);
+      dom.launchpage();
       return;
     }
     const _currentCheck = getCurrentTime();
@@ -41,7 +26,7 @@ const clock = () => {
       _currentDate,
       _currentCheck
     );
-    _currentDate.hours = _currentCheck.hours;
+
     if (secondChange) {
       const nextSecond =
         _currentCheck.seconds - 1 === -1 ? 59 : _currentCheck.seconds - 1;
@@ -58,20 +43,43 @@ const clock = () => {
       const nextHour =
         _currentCheck.hours - 1 === -1 ? 23 : _currentCheck.hours - 1;
       _currentDate.hours = _currentCheck.hours;
-      dom.changeHour(_currentCheck.hours, nextHour);
+      dom.checkTime("hour", _currentCheck.hours, nextHour);
     } else return;
     if (dayChange) {
-      const nextDay =
-        _currentCheck.days - 1 === -1 ? 364 : _currentCheck.hours - 1;
-      _currentDate.days = _currentCheck.hours;
-      dom.checkTime("hour", _currentCheck.days, nextDay);
+      const nextDay = _targetDay - 1 === -1 ? 364 : _targetDay - 1;
+      _currentDate.days = _targetDay;
+      dom.checkTime("day", _targetDay, nextDay);
     }
   };
 
-  const startCountdown = () => {
-    setInterval(changeTime, 1000);
-  };
-  startCountdown();
+  if (Date.now() >= _targetDate) {
+    // Set Inital Time wtih everythign set to 0
+    dom.setInitialTime({
+      second: 0,
+      nextSecond: 0,
+      minute: 0,
+      nextMinute: 0,
+      hour: 0,
+      nextHour: 0,
+      day: 0,
+      nextDay: 0,
+    });
+    dom.launchpage();
+  } else {
+    dom.setInitialTime({
+      second: _currentDate.seconds,
+      nextSecond:
+        _currentDate.seconds - 1 === -1 ? 59 : _currentDate.seconds - 1,
+      minute: _currentDate.minutes,
+      nextMinute:
+        _currentDate.minutes - 1 === -1 ? 59 : _currentDate.minutes - 1,
+      hour: _currentDate.hours,
+      nextHour: _currentDate.hours - 1 === -1 ? 23 : _currentDate.hours - 1,
+      day: _targetDay,
+      nextDay: _targetDay - 1 === -1 ? 364 : _targetDay - 1,
+    });
+    _startCountdown = setInterval(changeTime, 1000);
+  }
 };
 
 export default clock;
