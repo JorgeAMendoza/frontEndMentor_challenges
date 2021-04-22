9170b6d63a9cea1a5571280a283713d8ae3ed47a
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-04-22 09:25:07 -0500

    insert webpack configuration to be used by dev and production configuration

---
+++ blogr-landing-page/webpack.dev.js
@@ -0,0 +1,26 @@
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+});
