f88a35699faab8a28e8015b44028da5061b8541e
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-07-09 13:07:01 -0500

    create app js to start the application, index js starts it, set template html to have theme-1 as the default

---
+++ calculator-app/src/app/app.js
@@ -0,0 +1,6 @@
+import { themeSwitch } from "./theme-switch";
+
+// Instructions to start App
+(() => {
+  themeSwitch();
+})();
---
+++ calculator-app/src/app/theme-switch.js
@@ -0,0 +1,28 @@
+export const themeSwitch = () => {
+  const _staticDOM = {
+    pageBody: document.querySelector("body"),
+    themeSelect: document.querySelectorAll("input[type='radio']"),
+    themeTab: document.querySelector(".theme-select__tab"),
+  };
+
+  const changeTheme = (e) => {
+    const themeSelection = e.target.dataset.theme;
+    _staticDOM.pageBody.classList.remove("theme-1", "theme-2", "theme-3");
+
+    switch (themeSelection) {
+      case "1":
+        _staticDOM.pageBody.classList.add("theme-1");
+        break;
+      case "2":
+        _staticDOM.pageBody.classList.add("theme-2");
+        break;
+      case "3":
+        _staticDOM.pageBody.classList.add("theme-3");
+        break;
+    }
+  };
+
+  _staticDOM.themeSelect.forEach((input) =>
+    input.addEventListener("change", changeTheme)
+  );
+};
