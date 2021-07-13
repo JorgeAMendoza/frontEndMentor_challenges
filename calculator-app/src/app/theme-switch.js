bc8ad5bffdf90475a990b4716b8d0d176e80cce0
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-07-13 13:23:04 -0500

    whenever the theme is switched, save the theme name to locale storage to be used the next time the page is reloaded, if there is no last saved themme then defualt to theme one

--- calculator-app/src/app/theme-switch.js
+++ calculator-app/src/app/theme-switch.js
@@ -4,6 +4,7 @@ export const themeSwitch = () => {
     themeSelect: document.querySelectorAll("input[type='radio']"),
     themeTab: document.querySelector(".theme-select__tab"),
   };
+  const _setTheme = JSON.parse(localStorage.getItem("theme")) || "theme-1";
 
   const _changeTheme = (e) => {
     const themeSelection = e.target.dataset.theme;
@@ -12,12 +13,15 @@ export const themeSwitch = () => {
     switch (themeSelection) {
       case "1":
         _staticDOM.pageBody.classList.add("theme-1");
+        localStorage.setItem("theme", JSON.stringify("theme-1"));
         break;
       case "2":
         _staticDOM.pageBody.classList.add("theme-2");
+        localStorage.setItem("theme", JSON.stringify("theme-2"));
         break;
       case "3":
         _staticDOM.pageBody.classList.add("theme-3");
+        localStorage.setItem("theme", JSON.stringify("theme-3"));
         break;
     }
   };
@@ -25,4 +29,7 @@ export const themeSwitch = () => {
   _staticDOM.themeSelect.forEach((input) =>
     input.addEventListener("change", _changeTheme)
   );
+
+  // Set the theme
+  _staticDOM.pageBody.classList.add(_setTheme);
 };
