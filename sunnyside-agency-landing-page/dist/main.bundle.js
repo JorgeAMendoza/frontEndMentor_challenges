fdce4f62f62164e7b124ba0a6b4d6a65bfbc0229
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-07-24 12:01:20 -0500

    latest webpack build, dev

---
+++ sunnyside-agency-landing-page/dist/main.bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Hopefully all is well");
+console.log("hello world");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zdW5ueS1zaWRlLWxhbmRpbmctcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EiLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkhvcGVmdWxseSBhbGwgaXMgd2VsbFwiKTtcclxuY29uc29sZS5sb2coXCJoZWxsbyB3b3JsZFwiKTtcclxuIl0sInNvdXJjZVJvb3QiOiIifQ==
