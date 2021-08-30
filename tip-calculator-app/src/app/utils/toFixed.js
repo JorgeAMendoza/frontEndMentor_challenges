8dadea88d330488afb4576bd0fe3dfde97061848
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-08-30 18:14:47 -0500

    add more test for to fixed to test rounded and non roudned values, added the logic to tofixed to handle rounding values

--- tip-calculator-app/src/app/utils/toFixed.js
+++ tip-calculator-app/src/app/utils/toFixed.js
@@ -1,4 +1,5 @@
-export default function toFixed(num, fixed) {
-  const re = new RegExp('^-?\\d+(?:.\\d{0,' + (fixed || -1) + '})?');
+export default function toFixed(num, round) {
+  if (round) return Math.round(num * 100) / 100;
+  const re = new RegExp('^-?\\d+(?:.\\d{0,' + (2 || -1) + '})?');
   return Number(num.toString().match(re)[0]);
 }
