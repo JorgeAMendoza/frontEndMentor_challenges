4890234c79c60d2de07dfbd5c4610a5f52452fee
Jorge Mendoza <jorgemendoza2002@gmail.com>
2021-11-04 14:25:39 -0500

    create global styled component

---
+++ github-user-search-app/src/app/components/styles/Global.styled.js
@@ -0,0 +1,51 @@
+import * as styled from 'styled-components';
+
+export const GlobalStyles = styled.createGlobalStyle`
+  html {
+    font-size: 62.5%;
+    box-sizing: border-box;
+  }
+
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+    padding: 0;
+    margin: 0;
+    line-height: 1.4;
+  }
+
+  body {
+    font-family: 'Space Mono', monospace;
+    font-size: 1.6rem;
+    min-height: 100vh;
+  }
+
+  a,
+  button {
+    text-decoration: none;
+    cursor: pointer;
+    border: none;
+    font-family: inherit;
+  }
+
+  ul,
+  li {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    display: block;
+  }
+
+  p,
+  li,
+  h1,
+  h2,
+  h3,
+  h4 {
+    overflow-wrap: break-word;
+    hyphens: auto;
+  }
+`;
