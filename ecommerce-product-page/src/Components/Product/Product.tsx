ae3e464663ead208ca809afc0805fe4fb8d85023
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-01-19 17:43:31 -0600

    move all of previuos ProductInfo contnet into the Product component
    when thinking of state and the strucutre of the page, i thought it would make more sense to just put all of the product page content into the Product component rather than it break it apart, since it would not  be that much of a hassle to worry about state and values that are being tracked by the Priceand Quanitity components

--- ecommerce-product-page/src/Components/Product/Product.tsx
+++ ecommerce-product-page/src/Components/Product/Product.tsx
@@ -1,11 +1,28 @@
 import { ImageCarousel } from './ImageCarousel/ImageCarousel';
-import { ProductInfo } from './ProductInfo/ProductInfo';
+import { Price } from './ProductInfo/Price';
+import { Quantity } from './ProductInfo/Quantity';
+import { Cart } from '../Icons/Cart';
 
 export const Product = () => {
   return (
     <section>
       <ImageCarousel />
-      <ProductInfo />
+      <div>
+        <span>Sneaker Company</span>
+        <h2>Fall Limited Edition Sneakers</h2>
+        <p>
+          These low-profile sneakers are your perfect casual wear companion.
+          Featuring a durable rubber outer sole, they’ll withstand everything
+          the weather can offer.
+        </p>
+
+        <Price />
+        <Quantity />
+        <button>
+          <Cart />
+          Add to Cart
+        </button>
+      </div>
     </section>
   );
 };
