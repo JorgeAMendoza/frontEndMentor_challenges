09ea34764bb4ddbf47cef1a8521e797bd4f4d0d5
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-02-03 11:24:36 -0600

    add mobile styling for the main product page on mobile devices

--- ecommerce-product-page/src/Components/Product/ProductInfo/Price.tsx
+++ ecommerce-product-page/src/Components/Product/ProductInfo/Price.tsx
@@ -6,10 +6,14 @@ interface PriceProps {
 
 export const Price = ({ price, discountPercent, msrp }: PriceProps) => {
   return (
-    <div>
-      <p>{price}</p>
-      <span>{discountPercent}</span>
-      <span>{msrp}</span>
+    <div className="flex gap-4 align-middle">
+      <p className="font-bold text-2xl">${price}.00</p>
+      <span className="text-orange font-bold bg-orange bg-opacity-20 py-1 px-2 rounded-md">
+        {discountPercent}%
+      </span>
+      <span className="ml-auto line-through text-light-gray font-bold opacity-50">
+        ${msrp}.00
+      </span>
     </div>
   );
 };
