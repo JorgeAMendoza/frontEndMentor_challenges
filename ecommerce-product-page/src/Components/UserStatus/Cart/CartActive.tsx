3a063345fb332327c786327c1a11377ec540eea0
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-02-05 09:49:28 -0600

    add desktop styling to the cart, adjust height/width, and position as well

--- ecommerce-product-page/src/Components/UserStatus/Cart/CartActive.tsx
+++ ecommerce-product-page/src/Components/UserStatus/Cart/CartActive.tsx
@@ -7,8 +7,8 @@ export const CartActive = () => {
 
   const renderCart = cart.cartItems.length ? true : false;
   return (
-    <div className="absolute top-20 left-0 z-10 w-screen px-2">
-      <div className="bg-white rounded-md py-4 min-h-[65vw]">
+    <div className="absolute top-20 left-0 z-10 w-screen px-2 md:w-[25rem] md:top-16 md:-left-[19rem] xl:border-2">
+      <div className="bg-white rounded-md py-4 min-h-[65vw] md:min-h-[15rem]">
         <p className="font-bold px-5 pb-5 border-b-2">Cart</p>
         <div className="">
           {renderCart ? (
