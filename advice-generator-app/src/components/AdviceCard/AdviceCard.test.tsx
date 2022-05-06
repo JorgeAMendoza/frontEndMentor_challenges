d260d348d0c385a1704206667566c32b2c372606
Jorge Mendoza <jorgemendoza2002@gmail.com>
2022-05-06 07:10:40 -0500

    ran unit test on the advice card component and ensured that that it rendered the props correctly

---
+++ advice-generator-app/src/components/AdviceCard/AdviceCard.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react'
+import AdviceCard from './AdviceCard'
+
+describe('Advice card renders the correct props', () => {
+  const testData = {
+    adviceNumber: Math.floor(Math.random() * 100 + 1),
+    adviceText: 'Defeating a sandwhich, only makes it tastier',
+  }
+
+  test('card renders correct advice number and text', () => {
+    render(
+      <AdviceCard
+        adviceNumber={testData.adviceNumber}
+        adviceText={testData.adviceText}
+        adviceCardType="front"
+      />
+    )
+    screen.getByText(`Advice #${testData.adviceNumber}`)
+    screen.getByText(`"${testData.adviceText}"`)
+  })
+
+  test('card has test id of its type', () => {
+    render(
+      <AdviceCard
+        adviceNumber={testData.adviceNumber}
+        adviceText={testData.adviceText}
+        adviceCardType="front"
+      />
+    )
+
+    screen.getByTestId('front')
+  })
+})
